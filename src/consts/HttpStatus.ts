a142c5b94c55c5be3ea94bb5f7881b300992218d
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-14 11:28:11 -0300

    const http_status

---
+++ src/consts/HttpStatus.ts
@@ -0,0 +1,13 @@
+const HTTP_STATUS = {
+    OK: 200, // Requisição bem-sucedida
+    CREATED: 201, // Recurso criado com sucesso
+    BAD_REQUEST: 400, // Requisição inválida
+    UNAUTHORIZED: 401, // Usuário não autenticado
+    FORBIDDEN: 403, // Acesso negado
+    NOT_FOUND: 404, // Recurso não encontrado
+    METHOD_NOT_ALLOWED: 405, // Método não permitido
+    INTERNAL_SERVER_ERROR: 500, // Erro interno do servidor
+    BAD_GATEWAY: 502, // Gateway inválido
+};
+
+export default HTTP_STATUS;
