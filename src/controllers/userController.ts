bf4456ecb79b42937fc7b80a238b640929e0716d
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-14 14:37:14 -0300

    login

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,14 +1,16 @@
 import { RequestHandler } from "express";
-import { userSchema } from "../schemas/userSchema";
+import { loginSchema, registerUserSchema } from "../schemas/userSchema";
 import HTTP_STATUS from "../consts/HttpStatus";
-import { CreateUserModel } from "../models/userModel";
+import { CreateUserModel, findUser, updateUser } from "../models/userModel";
 import { sendVerificationCode } from "../services/sendVerificationCode";
-import createCode from "../services/createCode";
+import bcrypt from "bcrypt"
+import { updateCode } from "../models/codeModel";
+import { createJWT } from "../services/createJWT";
 
 export const CreateUser: RequestHandler = async (req, res) => {
 
     // pegar os dados
-    const dados = userSchema.safeParse(req.body)
+    const dados = registerUserSchema.safeParse(req.body)
 
     // verificar os dados
     if (dados.error) {
@@ -23,7 +25,7 @@ export const CreateUser: RequestHandler = async (req, res) => {
 
         const user = await CreateUserModel(data)
 
-        // verificar se oq chegou foi um erro ou o usuario e retornar um json com um erro ou com o usuario
+        // verificar se oq chegou foi um erro ou o usuario e retornar um json 
         if (user instanceof Error) {
             res.status(HTTP_STATUS.BAD_REQUEST).json({
                 message: user.message
@@ -65,8 +67,70 @@ export const CreateUser: RequestHandler = async (req, res) => {
 
 }
 
-export const verifyUser: RequestHandler = (req, res) => {
+export const login: RequestHandler = async (req, res) => {
 
     //pegar os dados de email, senha e o codigo
+    const dados = loginSchema.safeParse(req.body)
+
+    if (dados.success) {
+        try {
+            const user = await findUser(dados.data.email)
+
+            if (user && user.code && user.code.used === false) {
+                //verificar se o email corresponde a um usuário, se a senha corresponde a senha do usuario no banco de dados e se o codigo é o msm q foi enviado para o email
+                const comparedPassword = await bcrypt.compare(dados.data.password, user.password)
+                const comparedCode = user.code?.id === req.body.code
+                if (!comparedPassword) {
+                    res.status(HTTP_STATUS.BAD_REQUEST).json({
+                        message: "Dados Incorretos"
+                    })
+                    return
+                }
+                if (!comparedCode) {
+                    res.status(HTTP_STATUS.BAD_REQUEST).json({
+                        message: "Código Invalido"
+                    })
+                    return
+                }
+                //após verificar esses dados gerar um jwt com o email e o id do usuário e setar o status do usuário e do code para true
+                const token = createJWT(user)
+                res.status(HTTP_STATUS.OK).json({
+                    token
+                })
+                const updatedUser = await updateUser(user.id, { status: true, })
+
+                const updatedCode = await updateCode(user.code.id, { used: true, })
+
+                res.status(HTTP_STATUS.OK).json({ updatedUser, updatedCode })
+                return
+
+
+
+            }
+            if (user && user.status === true) {
+                const token = createJWT(user)
+
+                res.status(HTTP_STATUS.OK).json({
+                    token
+                })
+                return
+            }
+            throw new Error("Usuário Nao Encontrado")
+        } catch {
+            res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+                message: "Erro interno"
+            })
+            return
+        }
+    }
+    res.status(HTTP_STATUS.BAD_REQUEST).json({
+        message: dados.error?.flatten().fieldErrors
+    })
+    return
+
+
+
+
 
 }
+
