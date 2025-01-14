bda950cbfa5578c437af4e68388c38765cce48d4
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-14 11:29:34 -0300

    usando o helmet,cors e rota not found

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,19 @@ import express from "express"
 import dotenv from "dotenv"
 import helmet from "helmet"
 import cors from "cors"
+import route from "./routes/routes"
+
 
 dotenv.config()
 
 const porta = process.env.PORTA || 3000
 const server = express()
+
+server.use(helmet())
+
+server.use(cors({
+	origin: "*"
+}))
 
 server.use(express.urlencoded({
 	extended: true
@@ -17,6 +25,15 @@ server.use(express.json())
 server.use("/api/ping", (req, res) => {
 	res.status(200).json({
 		pong: true
+	})
+})
+
+server.use(route)
+
+
+server.use("", (req, res) => {
+	res.status(404).json({
+		message: "Route not found, if you have permission try /api/routes"
 	})
 })
 
