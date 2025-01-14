e9d2063fe06ecb3125be10d7fd479f50de188b06
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-14 15:58:31 -0300

    função para validar a senha e gerar o jwt

--- src/services/createJWT.ts
+++ src/services/createJWT.ts
@@ -1,8 +1,30 @@
 import JWT from "jsonwebtoken"
-
+import bcrypt from "bcrypt"
 
 
 export function createJWT(user: JWT) {
     const token = JWT.sign({ email: user.email, id: user.id }, process.env.MY_SECRET_KEY as string)
     return token
+}
+
+export async function validatePasswordToCreateJWT(password: string, user: CompareJWT) {
+    try {
+        const comparedPassword = await bcrypt.compare(password, user.password)
+        if (!comparedPassword) {
+            throw new Error("Senha Incorreta")
+
+        }
+        const token = createJWT(user)
+
+        return token
+
+    }
+    catch (e) {
+        if (e instanceof Error) {
+            return new Error(e.message)
+        }
+        throw new Error("Erro Interno")
+
+    }
+
 }
