7507880b2d41811e7fc564455efeb4c44c509af5
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-19 19:16:35 -0300

    novas rotas

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { CreateUser, deleteUser, login } from "../controllers/userController"
+import { CreateUser, deleteUser, getUser, login } from "../controllers/userController"
 import { authMiddleware } from "../middlewares/authMiddleware"
 import { verifyCode } from "../controllers/codeController"
 
@@ -38,9 +38,35 @@ route.post("/api/sign", CreateUser)
 
 route.post("/api/code", verifyCode)
 
+route.get("/api/user", authMiddleware, getUser)
+
 route.post("/api/login", login)
+
+
+
+
+
+
+
+
 
 route.delete("/api/user", authMiddleware, deleteUser)
 
+route.post("/api/produtos", authMiddleware, adminMiddleware, createProduct)
+
+route.put("/api/produto:id", authMiddleware, adminMiddleware, updateProduct)
+
+route.delete("/api/produto:id", authMiddleware, adminMiddleware, deleteProduct)
+
+
+
+
+
+
+route.post("/api/cart:produtoId", authMiddleware, addToCart)
+
+route.delete("/api/cart:produtoId", authMiddleware, removeFromCart)
+
+route.put("/api/cart:produtoId", authMiddleware, updateFromCart)
 
 export default route
