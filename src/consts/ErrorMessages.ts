3e58646590eb88294c5e9c3fda94b0bb3f4e5413
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-21 17:02:33 -0300

    schema produtos

--- src/consts/ErrorMessages.ts
+++ src/consts/ErrorMessages.ts
@@ -15,7 +15,22 @@ const ErrorMessage = {
 
     idSizeError: "Id não contém 24 digitos",
 
-    tokenSizeError: "Token não contém 172 caracteres"
+    tokenSizeError: "Token não contém 172 caracteres",
+
+    productNameSizeError: "O nome do produto deve conter pelo menos 2 caracteres.",
+
+    productPriceSizeError: "O preço do produto  deve conter 7 caracteres totais no formato R$00,00 .",
+
+    productSizeError: "O tamanho do produto  deve conter pelo menos 1 tamanho com no minimo 1 digito",
+
+    productColorNameSizeError: "O nome da cor do produto deve conter pelo menos 3 caracteres",
+
+    productColorHexMaxSizeError: "O código da cor do produto deve conter no maximo 7 caracteres.",
+
+    productColorHexMinSizeError: "O código da cor do produto deve conter no minimo 4 caracteres.",
+
+    productCategoryError: "A categoria do produto deve ser um array de strings com pelo menos uma categoria."
+
 }
 
 
---
+++ src/schemas/productSchema.ts
@@ -0,0 +1,20 @@
+import { z } from "zod"
+import ErrorMessage from "../consts/ErrorMessages";
+
+export const productSchema = z.object({
+
+    name: z.string().min(2, ErrorMessage.productNameSizeError),
+    price: z.string().min(7, ErrorMessage.productPriceSizeError),
+    size: z.array(z.string({ required_error: ErrorMessage.productSizeError })),
+    photo: z.string(),
+    colorName: z.string().min(3, ErrorMessage.productColorNameSizeError),
+    colorHex: z.string().min(4, ErrorMessage.productColorHexMinSizeError).max(7, ErrorMessage.productColorHexMaxSizeError),
+    frete: z.string(),
+    category: z.array(z.string().min(1, ErrorMessage.productCategoryError)),
+
+});
+
+
+
+
+
